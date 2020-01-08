ecf94ca4b64e9c877269fae910c52e1de953fe58
Kevin Kredit <k.kredit.us@ieee.org>
2020-01-07 22:45:38 -0500

    Add Go installation, Makefile, and hello-world

---
+++ go/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+URL="$(curl -s https://golang.org/dl/ | grep "linux-amd64.tar.gz" | head -1 | cut -d\" -f4)"
+FILE="$(echo $URL | rev | cut -d/ -f1 | rev)"
+INST_DIR=/usr/local
+
+echo "Downloading $URL..."
+wget -q --show-progress $URL
+echo "Untarring $FILE into $INST_DIR..."
+sudo tar -C $INST_DIR -xzf $FILE
+
+if echo $PATH | grep -v "go/bin" &>/dev/null; then
+    echo "Adding $INST_DIR/go/bin to ~/.profile..."
+    echo "export PATH=\$PATH:$INST_DIR/go/bin" >> ~/.profile
+    echo "Run 'export PATH=\$PATH:$INST_DIR/go/bin'"
+fi
