e8f45d5382570e93ea78e1dee09d5bbe575ad681
Kevin Kredit <k.kredit.us@ieee.org>
2020-02-16 15:34:29 -0500

    Add haskell modules section

--- haskell/06-modules.hs
+++ haskell/06-modules.hs
@@ -7,5 +7,14 @@
 module Main
   where
 
-main = putStrLn "This file meant to be loaded in GHCI"
+-- Imports must be before defining any functions but after 'module Main'
+import Data.List
+import qualified Data.Map as M
+import qualified Geometry.Sphere as Sphere
+import qualified Geometry.Cuboid as Cuboid
+import qualified Geometry.Cube as Cube
 
+main = print $ Sphere.area 5
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
