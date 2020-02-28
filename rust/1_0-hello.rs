d15e1b7098ff4acf013be68d982124af2c20cb03
Kevin Kredit <k.kredit.us@ieee.org>
2020-02-28 10:05:19 -0500

    Rust: create setup and hello-world

---
+++ rust/1_0-hello.rs
@@ -0,0 +1,21 @@
+/* Original source by the Rust Team
+ * Modifications by Kevin Kredit
+ * Licensed under https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * https://doc.rust-lang.org/stable/rust-by-example/hello.html
+ */
+
+// This is a comment, and is ignored by the compiler
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
---
+++ rust/helpers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function mergewith() {
+    STR="$2"
+    for ELEM in "${@:3}"; do
+        STR="${STR}${1}${ELEM}"
+    done
+    echo $STR
+}
+
+function nr() {
+    PREFIX=${1//./_} # the number
+    NAME=$(mergewith "-" ${@:2})
+    URI=$(mergewith "/" ${@:2})
+
+    FILE=$PREFIX-$NAME.rs
+    cat > $FILE <<EOF
+/* Original source by the Rust Team
+ * Modifications by Kevin Kredit
+ * Licensed under https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * https://doc.rust-lang.org/stable/rust-by-example/$URI.html
+ */
+
+EOF
+    git add $FILE
+    codium $FILE
+}
